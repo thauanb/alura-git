4c695129f7e9f77d975b3793a40cb0bf4d890c76
thauanb <thauanb@gmail.com>
2023-09-12 04:40:59 -0300

    fix: Refactoring battery information in `battery.py`

--- battery.py
+++ battery.py
@@ -1,29 +1,32 @@
-import psutil , time
+import psutil
+import time
 from os import system
 from plyer import notification
 from colorama import Fore, Style
 
+
 # Set the colors
 green = Fore.GREEN
 red = Fore.RED
-style_reset =Style.RESET_ALL
-
+yellow = Fore.YELLOW
+style_reset = Style.RESET_ALL
 # Symbols
 simbol_ok = f'[{green}+{style_reset}]'
 simbol_fail = f'[{red}-{style_reset}]'
+simbol_loading = f'[{red}.{style_reset}]'
 simbol_exclamation = f'[{red}!{style_reset}]'
 
 
-
-def limparTela():
+def limparTela() -> None:
     system('cls')
 
-def check():
+
+def check() -> bool:
     '''
     Check if the battery is 100% and if it is connected to the power supply    
     '''
     try:
-        battery   = psutil.sensors_battery()
+        battery = psutil.sensors_battery()
     except:
         print(f'{simbol_fail} Erro ao verificar a bateria')
         battery = None
@@ -31,22 +34,25 @@ def check():
         plugged = battery.power_plugged
     except:
         print(f'{simbol_fail} Erro ao verificar se está conectado')
-             
-    percent = int(battery.percent)
-    isplugged = f"{simbol_ok} Conectado" if plugged==True else f"{simbol_fail} Desconectado"
-    print(f'{simbol_ok} Bateria   -> {percent}')
 
-    if (percent == 100) and (plugged==True):
-        notification.notify(title='Bateria  100%',message='A bateria está completamente carregada',timeout=100)
+    percent = int(battery.percent)
+    isplugged = f"{simbol_ok} Conectado" if plugged == True else f"{simbol_fail} Desconectado"
+    if (percent <= 50):
+        print(f'{simbol_exclamation} Bateria   -> {red}{percent}{style_reset} %')
+    elif (percent > 50):
+        print(f'{simbol_loading} Bateria   -> {yellow}{percent}{style_reset} %')
+
+    if (percent == 100) and (plugged == True):
+        notification.notify(
+            title='Bateria  100%', message='A bateria está completamente carregada', timeout=100)
         system('shutdown /s /t 0"')
         input()
         return True
     return False
 
 
-
-def run(mode:bool,minutes_to_wait:int=5):
-    while mode==True:
+def run(mode: bool, minutes_to_wait: int = 5):
+    while mode == True:
         limparTela()
         print('======== Battery Saver ========\n ')
         check()
@@ -55,7 +61,7 @@ def run(mode:bool,minutes_to_wait:int=5):
         time.sleep(minutes_to_wait*60)
         check()
 
+
 if __name__ == '__main__':
     system('title Battery Saver')
-    run(True,10)
-
+    run(True, 10)
